866bdc82846b173d2f575dd707b079347a048126
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-06 11:25:35 -0400

    inital uploading

---
+++ Rune.py
@@ -0,0 +1,108 @@
+from PIL import Image, ImageDraw
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+ALPHA = "abcdefghijklmnopqrstuvwxyz" 
+
+class RuneGen(object):
+
+	"""docstring for RuneGen"""
+
+	def __init__(self, inx,iny,inw):
+		self.x = inx
+		self.y = iny
+		self.w = inw
+		self.points = []
+		self.points.append((lambda t: (t.x,t.y)))
+		self.lines = []
+		self.name = ""
+
+
+	def set_name(self,s):
+		self.name = s
+
+	# add a point to the list of points
+	def add_point(self,p):
+		self.points.append(p)
+
+
+	# append a list of points to the existing point list
+	def add_point_list(self,pl):
+		self.points.extend(pl)
+
+
+	# Add a line to the list of lines
+	def add_line(self,p1,p2):
+		self.lines.append((p1,p2))
+
+
+	def add_line_list(self, line_list):
+		self.lines.extend(line_list)
+
+
+	# LINE MAKER?
+	def line_create(self,coord):
+		p1 = self.points[coord[0]]
+		p2 = self.points[coord[1]]
+		self.lines.append((p1,p2))
+
+
+	def line_gen(self,line_point_list):
+		for l in line_point_list:
+			self.line_create(l)
+
+
+	# calculates the current values of the point
+	def calculate_point(self,p):
+		return p(self)
+
+	# calculates the entire list
+	def calculate_point_list(self):
+		return [self.calculate_point(p) for p in self.points]
+
+
+	def calculate_line(self,l):
+		return self.calculate_point(l[0]),self.calculate_point(l[1])
+
+
+	def calculate_line_list(self):
+		return [self.calculate_line(l) for l in self.lines]
+
+
+	def rune_dimensions(self):
+		c = self.calculate_point_list()
+		max_x = max(c, key = lambda i: i[0])[0]
+		min_x = min(c, key = lambda i: i[0])[0]
+		# print("x: ", max_x,min_x)
+		max_y = max(c, key = lambda i: i[1])[1]
+		min_y = min(c, key = lambda i: i[1])[1]
+		# print("y: ", max_y,min_y)
+		x = max_x - min_x
+		y = max_y - min_y
+		return (x, y)
+
+
+	def rune_size(self):
+		(x_wid, y_wid) = self.rune_dimensions()
+		x_wid += self.x * 2
+		y_wid += self.y * 2
+		return (x_wid, y_wid)
+
+
+	def draw_points(self):
+		im = Image.new("RGB", self.rune_size(), WHITE)
+		dr = ImageDraw.Draw(im)
+		dr.point(self.calculate_point_list(),BLACK)
+		im.save("images/points_{}.png".format(self.name))
+
+
+	def draw_lines(self):
+		im = Image.new("RGB", self.rune_size(), WHITE)
+		dr = ImageDraw.Draw(im)
+		for li in self.calculate_line_list():
+			dr.line(li,BLACK)
+		im.save("images/lines_{}.png".format(self.name))
+
+
+
+
---
+++ main.py
@@ -0,0 +1,96 @@
+# from PointGen import *
+from Rune import RuneGen
+from math import sqrt, floor
+x = 5
+y = 5
+w = 40
+
+ruMirror = RuneGen(x,y,w)
+ruHex = RuneGen(x,y,w)
+ruRhombus = RuneGen(x,y,w)
+
+# pg = PointGen(x,y,w)
+
+# lam = lambda a: a(pg)
+
+point_list_mirror = [
+	lambda t: (t.x,t.y),
+	lambda t: (t.x + t.w, t.y),
+	lambda t: (t.x, t.y + t.w),
+	lambda t: (t.x + t.w, t.y + t.w),
+	lambda t: (t.x, t.y + 2 * t.w),
+	lambda t: (t.x + t.w, t.y + 2 * t.w)
+]
+
+point_list_hex = [
+	lambda t: (t.x + t.w//2, t.y),
+	lambda t: (t.x + (3*t.w//2), t.y),
+	lambda t: (t.x, t.y + floor(sqrt(3)*t.w/2)),
+	lambda t: (t.x + t.w, t.y + floor(sqrt(3)*t.w/2)),
+	lambda t: (t.x + 2*t.w, t.y + (sqrt(3)*t.w/2)),
+	lambda t: (t.x + t.w//2, t.y + floor(sqrt(3)*t.w)),
+	lambda t: (t.x + (3*t.w//2), t.y + floor(sqrt(3)*t.w))
+]
+
+point_list_rhombus = [
+	lambda t: (t.x + t.w//2, t.y),
+	lambda t: (t.x, t.y + floor(sqrt(3)*t.w/2)),
+	lambda t: (t.x + t.w, t.y + floor(sqrt(3)*t.w/2)),
+	lambda t: (t.x + t.w//2, t.y + floor(sqrt(3)*t.w))
+]
+
+line_list_mirror = [
+	(1,2),
+	(1,3),
+	(1,4),
+	(2,3),
+	(2,4),
+	(3,4),
+	(3,5),
+	(3,6),
+	(4,5),
+	(4,6),
+	(5,6)
+]
+
+line_list_rhombus = [(1,2),(1,3),(2,3),(2,4),(3,4)]
+
+line_list_hex = [
+	(1,2),
+	(1,3),
+	(1,4),
+	(2,4),
+	(2,5),
+	(3,4),
+	(3,6),
+	(4,5),
+	(4,6),
+	(4,7),
+	(5,7),
+	(6,7)]
+
+# Mirror Rune Setup
+ruMirror.add_point_list(point_list_mirror)
+ruMirror.set_name("mirror_rune")
+ruMirror.line_gen(line_list_mirror)
+
+# Hex Rune Setup
+ruHex.add_point_list(point_list_hex)
+ruHex.set_name("hex_rune")
+ruHex.line_gen(line_list_hex)
+
+# Rhombus Rune Setup
+ruRhombus.add_point_list(point_list_rhombus)
+ruRhombus.set_name("rhombus_rune")
+ruRhombus.line_gen(line_list_rhombus)
+
+
+# Draw the points
+ruHex.draw_points()
+ruMirror.draw_points()
+ruRhombus.draw_points()
+
+#make a test line
+ruMirror.draw_lines()
+ruRhombus.draw_lines()
+ruHex.draw_lines()
